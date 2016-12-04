76331277ee9326af9d06ca5f6d7afa6c84033dd1
Boyko Anton <boyko.ant@live.com>
2016-12-04 18:36:15 +0000

    added kilometer and day multiplier for car types

--- CarRentalDatabase/CarRentalDatabase.MSSQL/Tables/CarTypes.sql
+++ CarRentalDatabase/CarRentalDatabase.MSSQL/Tables/CarTypes.sql
@@ -1,7 +1,11 @@
-﻿CREATE TABLE [dbo].[CarTypes]
-(
-	[Id] TINYINT NOT NULL PRIMARY KEY IDENTITY(1,1), 
-    [Name] NVARCHAR(50) NOT NULL, 
-    [DayPrice] DECIMAL(9, 2) NOT NULL, 
-    [KilometerPrice] DECIMAL(9, 2) NOT NULL
-)
+﻿CREATE TABLE [dbo].[CarTypes] (
+    [Id]                  TINYINT        IDENTITY (1, 1) NOT NULL,
+    [Name]                NVARCHAR (50)  NOT NULL,
+    [DayPrice]            DECIMAL (9, 2) NOT NULL,
+    [DayMultiplier]       DECIMAL (5, 2) NOT NULL,
+    [KilometerPrice]      DECIMAL (9, 2) NOT NULL,
+    [KilometerMultiplier] DECIMAL (5, 2) NOT NULL,
+    CONSTRAINT [PK__CarTypes__3214EC070C4BDBA6] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
