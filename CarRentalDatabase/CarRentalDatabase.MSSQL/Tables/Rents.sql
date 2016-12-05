12944f5aded932f23d9c6ffc629bd01dd15028e9
Boyko Anton <boyko.ant@live.com>
2016-12-05 11:10:13 +0000

    introduce registration number for rents

--- CarRentalDatabase/CarRentalDatabase.MSSQL/Tables/Rents.sql
+++ CarRentalDatabase/CarRentalDatabase.MSSQL/Tables/Rents.sql
@@ -1,21 +1,24 @@
 ﻿CREATE TABLE [dbo].[Rents] (
-    [Id]              INT            IDENTITY (1, 1) NOT NULL,
-    [ClientId]        INT            NOT NULL,
-    [BookingId]       INT            NOT NULL,
-    [CarTypeId]       INT            NOT NULL,
-    [DateFrom]        SMALLDATETIME  NOT NULL,
-    [DateTo]          SMALLDATETIME  NULL,
-    [InitialMileage]  DECIMAL (9, 2) NOT NULL,
-    [FinalMileage]    DECIMAL (9, 2) NULL,
-    [FinalPrice]      DECIMAL (9, 2) NULL,
-    [FullName]        NVARCHAR (100) NOT NULL,
-    [Phones]          NVARCHAR (50)  NOT NULL,
-    [InsuranceNumber] NVARCHAR (50)  NOT NULL,
+    [Id]                 INT            IDENTITY (1, 1) NOT NULL,
+    [ClientId]           INT            NOT NULL,
+    [BookingId]          INT            NOT NULL,
+    [CarTypeId]          INT            NOT NULL,
+    [RegistrationNumber] NVARCHAR (20)  NOT NULL,
+    [DateFrom]           SMALLDATETIME  NOT NULL,
+    [DateTo]             SMALLDATETIME  NULL,
+    [InitialMileage]     DECIMAL (9, 2) NOT NULL,
+    [FinalMileage]       DECIMAL (9, 2) NULL,
+    [FinalPrice]         DECIMAL (9, 2) NULL,
+    [FullName]           NVARCHAR (100) NOT NULL,
+    [Phones]             NVARCHAR (50)  NOT NULL,
+    [InsuranceNumber]    NVARCHAR (50)  NOT NULL,
     CONSTRAINT [PK_Rents] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_Rents_Bookings] FOREIGN KEY ([BookingId]) REFERENCES [dbo].[Bookings] ([Id]),
     CONSTRAINT [FK_Rents_CarTypes] FOREIGN KEY ([CarTypeId]) REFERENCES [dbo].[CarTypes] ([Id]),
     CONSTRAINT [FK_Rents_Clients] FOREIGN KEY ([ClientId]) REFERENCES [dbo].[Clients] ([Id])
 );
+
+
 
 
 
