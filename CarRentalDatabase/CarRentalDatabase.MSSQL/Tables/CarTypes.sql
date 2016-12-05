2c7f1b6f1b44b722c5880605ada8ce90e7db3b54
Boyko Anton <boyko.ant@live.com>
2016-12-05 11:09:51 +0000

    introduce settings table

--- CarRentalDatabase/CarRentalDatabase.MSSQL/Tables/CarTypes.sql
+++ CarRentalDatabase/CarRentalDatabase.MSSQL/Tables/CarTypes.sql
@@ -1,12 +1,12 @@
 ﻿CREATE TABLE [dbo].[CarTypes] (
     [Id]                  INT            IDENTITY (1, 1) NOT NULL,
     [Name]                NVARCHAR (50)  NOT NULL,
-    [DayPrice]            DECIMAL (9, 2) NOT NULL,
     [DayMultiplier]       DECIMAL (5, 2) NOT NULL,
-    [KilometerPrice]      DECIMAL (9, 2) NOT NULL,
     [KilometerMultiplier] DECIMAL (5, 2) NOT NULL,
     CONSTRAINT [PK_CarTypes] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
 
 
---
+++ CarRentalDatabase/CarRentalDatabase.MSSQL/dbo/Tables/Settings.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Settings] (
+    [Id]             INT            IDENTITY (1, 1) NOT NULL,
+    [DayPrice]       DECIMAL (9, 2) NOT NULL,
+    [KilometerPrice] DECIMAL (9, 2) NOT NULL,
+    CONSTRAINT [PK_Settings] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
